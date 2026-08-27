42f9caab23eb69c2a7de492bd66e6ad71a40b5ac
Arthur <arthurhleocadio@gmail.com>
2026-08-26 22:41:23 -0300

    Update sha3_256.hpp

--- sha3_256.hpp
+++ sha3_256.hpp
@@ -2,5 +2,11 @@
 
 struct Sha3_256{
   uint64_t state[5][5] = {0};
-  uin64_t rorl64(uint64_t x, unsigned int n);
+  uin64_t rotl64(uint64_t x, unsigned int n)
+{
+//PESQUISAR COMO ROTACIONAR
+}
+
+
+
 };
