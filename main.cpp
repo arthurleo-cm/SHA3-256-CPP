8968878e76af636ea3d809a6ab67bd83ea108511
Arthur <arthurhleocadio@gmail.com>
2026-08-30 22:49:35 -0300

    Update main.cpp
    Testando a função theta

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include "sha3_256.hpp"
+#include <cstdio>
 
+void print_state(const Sha3_256& h) {
+    for (int x = 0; x < 5; ++x)
+        for (int y = 0; y < 5; ++y)
+            printf("state[%d][%d] = %016lx\n", x, y, (unsigned long)h.state[x][y]);
+}
 
 int main()
 {
-  Sha3_256 hasher;
-  return 0;
+    Sha3_256 hasher;
+
+    // valores conhecidos, só para poder ver o efeito de cada etapa
+    uint64_t v = 1;
+    for (int x = 0; x < 5; ++x)
+        for (int y = 0; y < 5; ++y) {
+            hasher.state[x][y] = v;
+            v <<= 1;
+        }
+
+    printf("Antes do theta:\n");
+    print_state(hasher);
+
+    hasher.theta();
+
+    printf("\nDepois do theta:\n");
+    print_state(hasher);
+
+    return 0;
 }
